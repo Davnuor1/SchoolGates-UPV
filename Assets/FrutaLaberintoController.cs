206dc8cd47f4b0ac1fdb3479a1191f34d592c5fe
Davnuor1 <davnuor@gmail.com>
2024-06-11 23:47:05 +0200

    work david 11-06 final

---
+++ Assets/FrutaLaberintoController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class FrutaLaberintoController : MonoBehaviour
+{
+    private Transform player;
+    public float followSpeed = 50f;
+    public float followDistance = 1f; // Distancia deseada entre la fruta y el jugador
+    private bool isFollowing = false;
+    private Vector3 velocity = Vector3.zero;
+    
+
+    private void Update()
+    {
+        if (isFollowing && player != null)
+        {
+            // Calcular la distancia actual entre la fruta y el jugador
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+            // Calcular la posición objetivo
+            Vector3 targetPosition = player.position - (player.position - transform.position).normalized * followDistance;
+
+            // Mover la fruta hacia la posición objetivo con una velocidad específica
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, followSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Asegurarse de que solo el jugador puede hacer que la fruta lo siga
+        
+            player = other.transform;
+            isFollowing = true;
+        
+    }
+}
