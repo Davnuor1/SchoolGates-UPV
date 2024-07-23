5b181dcd16d232561af6c82fdcfaa9e716faaccf
Davnuor1 <davnuor@gmail.com>
2024-07-23 20:35:29 +0200

    work david 23-07 p2

---
+++ Assets/Scripts/MinijuegoContrase.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinijuegoContrase : MonoBehaviour
+{
+    [SerializeField] public GameObject minijuegoEntero;
+    [SerializeField] public Button botonCiencias;
+    [SerializeField] public Button botonArtes;
+    [SerializeField] public Button botonMatematicas;
+    [SerializeField] public Button botonHistoria;
+    [SerializeField] public Button botonHumanidades;
+    [SerializeField] public Button botonInformatica;
+
+
+
+
+
+
+
+    public void darBotonAsignatura(Button button)
+    {
+        if (button.name == "Ciencias")
+        {
+
+        }
+    }
+
+}
