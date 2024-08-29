0c0f5e957240aab5dc900173c697cc6a52b66a34
Davnuor1 <davnuor@gmail.com>
2024-08-29 22:38:15 +0200

    trabajo David 29 agosto final

--- Assets/Scripts/MagicCrystal.cs
+++ Assets/Scripts/MagicCrystal.cs
@@ -1,46 +1,42 @@
+using System.Collections;
 using UnityEngine;
 
 public class MagicCrystal : MonoBehaviour
 {
-    
     public CrystalSpriteChanger crystalSpriteChanger;
     private bool areRedCrystalsActive = false; // Estado inicial: rojos desactivados, azules activados
-
+    public float delay =0f; // Retardo 
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && WithinInteractionRange())
         {
-            ToggleCrystals();
+            StartCoroutine(ToggleCrystalsWithDelay());
         }
     }
 
     private bool WithinInteractionRange()
     {
-        // Asume que tienes una referencia al jugador, aquí solo necesitas
-        // verificar si está lo suficientemente cerca para interactuar.
         return Vector3.Distance(transform.position, GameManager.instance.player.transform.position) < 2f;
     }
 
-    private void ToggleCrystals()
+    private IEnumerator ToggleCrystalsWithDelay()
     {
+        yield return new WaitForSeconds(delay); // Esperar por el retardo especificado antes de ejecutar la lógica de toggle
+
         if (areRedCrystalsActive)
         {
             // Si los cristales rojos están activos, desactívalos y activa los azules
-            
             crystalSpriteChanger.AnimarAzulesEmerger();
             crystalSpriteChanger.AnimarRojosSumerger();
-            
-            
         }
         else
         {
             // Si los cristales rojos están inactivos, actívalos y desactiva los azules
             crystalSpriteChanger.AnimarRojosEmerger();
             crystalSpriteChanger.AnimarAzulesSumerger();
-            ;
-            
         }
+
         // Invierte el estado de los cristales rojos
         areRedCrystalsActive = !areRedCrystalsActive;
     }
