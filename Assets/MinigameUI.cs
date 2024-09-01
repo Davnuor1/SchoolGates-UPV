2ab39f6a944737b41c8dcabd6a9244193c325ca9
Davnuor1 <davnuor@gmail.com>
2024-09-01 23:55:18 +0200

    cambio extra

--- Assets/MinigameUI.cs
+++ Assets/MinigameUI.cs
@@ -6,8 +6,12 @@ public class MinigameUI : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject UIminijuego;
+    private PlayerMovement playerMovement;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         UIminijuego.SetActive(true);
+        playerMovement=GameManager.instance.player.GetComponent<PlayerMovement>();
+        playerMovement.enabled = false;
+        this.gameObject.SetActive(false);
     }
 }
