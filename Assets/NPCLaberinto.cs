44cb11de08ae4490a9f293d07e92d7ffea2418fa
Davnuor1 <davnuor@gmail.com>
2024-09-03 18:15:58 +0200

    work david 03 09 p1

--- Assets/NPCLaberinto.cs
+++ Assets/NPCLaberinto.cs
@@ -1,12 +1,47 @@
 using System.Collections;
 using UnityEngine;
+using PixelCrushers.DialogueSystem; // Asegúrate de incluir este namespace
 
 public class NPCLaberinto : MonoBehaviour
 {
-    
     public GameObject emoticono; // Referencia al objeto emoticono
 
+    void Start()
+    {
+        // Registrar el método EntregarFruta para que sea accesible desde Lua
+        Lua.RegisterFunction("EntregarFruta", this, this.GetType().GetMethod("EntregarFruta"));
+
+        // Registrar el método DeactivateEmoticonAfterDelay si también necesitas llamarlo directamente (poco común)
+        Lua.RegisterFunction("DeactivateEmoticonAfterDelay", this, this.GetType().GetMethod("DeactivateEmoticonAfterDelay"));
+
+        Lua.RegisterFunction("HayFrutaSiguiendo", this, this.GetType().GetMethod("HayFrutaSiguiendo"));
+    }
+
     private void OnMouseDown()
+    {
+        // Iniciar una conversación
+        DialogueManager.StartConversation("NPCFruta", transform);
+    }
+
+    public bool HayFrutaSiguiendo()
+    {
+        // Encuentra todas las frutas en la escena
+        FrutaLaberintoController[] frutas = FindObjectsOfType<FrutaLaberintoController>();
+
+        // Busca la fruta que está siguiendo al jugador
+        foreach (FrutaLaberintoController fruta in frutas)
+        {
+            if (fruta.isFollowing)
+            {
+                return true; // Retorna verdadero si hay al menos una fruta siguiendo al jugador
+            }
+        }
+        return false; // Retorna falso si no se encontró ninguna fruta siguiendo al jugador
+    }
+
+
+    // Método público que puede ser llamado desde el sistema de diálogo
+    public void EntregarFruta()
     {
         // Encuentra todas las frutas en la escena
         FrutaLaberintoController[] frutas = FindObjectsOfType<FrutaLaberintoController>();
@@ -19,9 +54,10 @@ public class NPCLaberinto : MonoBehaviour
                 // Desactivar la fruta
                 fruta.DeactivateFruit();
 
-                // Activar el emoticono en la posición de la caja
-                emoticono.transform.position = transform.position + new Vector3(0, 1, 0); // Ajusta la posición según sea necesario
+                // Activar el emoticono en la posición sobre el NPC
+                emoticono.transform.position = transform.position + new Vector3(0, 1, 0);
                 emoticono.SetActive(true);
+
                 // Iniciar la corrutina para desactivar el emoticono después de 2 segundos
                 StartCoroutine(DeactivateEmoticonAfterDelay());
 
@@ -30,10 +66,10 @@ public class NPCLaberinto : MonoBehaviour
         }
     }
 
-    private IEnumerator DeactivateEmoticonAfterDelay()
+    public IEnumerator DeactivateEmoticonAfterDelay()
     {
         // Esperar 2 segundos
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(3f);
 
         // Desactivar el emoticono
         emoticono.SetActive(false);
--- Assets/Scripts/StatsManager.cs
+++ Assets/Scripts/StatsManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using PixelCrushers.DialogueSystem;
 
 public class StatsManager : MonoBehaviour
 {
@@ -7,7 +8,49 @@ public class StatsManager : MonoBehaviour
     private void Awake()
     {
         playerStats = new PlayerStats(); // Inicializa las estadísticas del jugador
+        RegisterFunctions();
     }
+
+    private void OnDestroy()
+    {
+        UnregisterFunctions();
+    }
+
+    #region Lua Registration
+
+    private void RegisterFunctions()
+    {
+        Lua.RegisterFunction("ModifyEnergy", this, SymbolExtensions.GetMethodInfo(() => ModifyEnergy(0)));
+        Lua.RegisterFunction("ModifyKarma", this, SymbolExtensions.GetMethodInfo(() => ModifyKarma(0)));
+        Lua.RegisterFunction("ModifySpirituality", this, SymbolExtensions.GetMethodInfo(() => ModifySpirituality(0)));
+        Lua.RegisterFunction("ModifyExperience", this, SymbolExtensions.GetMethodInfo(() => ModifyExperience(0)));
+        Lua.RegisterFunction("SetEnergy", this, SymbolExtensions.GetMethodInfo(() => SetEnergy(0)));
+        Lua.RegisterFunction("SetKarma", this, SymbolExtensions.GetMethodInfo(() => SetKarma(0)));
+        Lua.RegisterFunction("SetSpirituality", this, SymbolExtensions.GetMethodInfo(() => SetSpirituality(0)));
+        Lua.RegisterFunction("SetExperience", this, SymbolExtensions.GetMethodInfo(() => SetExperience(0)));
+        Lua.RegisterFunction("GetEnergy", this, SymbolExtensions.GetMethodInfo(() => GetEnergy()));
+        Lua.RegisterFunction("GetKarma", this, SymbolExtensions.GetMethodInfo(() => GetKarma()));
+        Lua.RegisterFunction("GetSpirituality", this, SymbolExtensions.GetMethodInfo(() => GetSpirituality()));
+        Lua.RegisterFunction("GetExperience", this, SymbolExtensions.GetMethodInfo(() => GetExperience()));
+    }
+
+    private void UnregisterFunctions()
+    {
+        Lua.UnregisterFunction("ModifyEnergy");
+        Lua.UnregisterFunction("ModifyKarma");
+        Lua.UnregisterFunction("ModifySpirituality");
+        Lua.UnregisterFunction("ModifyExperience");
+        Lua.UnregisterFunction("SetEnergy");
+        Lua.UnregisterFunction("SetKarma");
+        Lua.UnregisterFunction("SetSpirituality");
+        Lua.UnregisterFunction("SetExperience");
+        Lua.UnregisterFunction("GetEnergy");
+        Lua.UnregisterFunction("GetKarma");
+        Lua.UnregisterFunction("GetSpirituality");
+        Lua.UnregisterFunction("GetExperience");
+    }
+
+    #endregion
 
     public void ModifyEnergy(int amount)
     {
@@ -47,5 +90,24 @@ public class StatsManager : MonoBehaviour
     public void SetExperience(int amount)
     {
         playerStats.experience = Mathf.Clamp(amount, playerStats.minExperience, playerStats.maxExperience);
+    }
+    public int GetEnergy()
+    {
+        return playerStats.energy;
+    }
+
+    public int GetKarma()
+    {
+        return playerStats.karma;
+    }
+
+    public int GetSpirituality()
+    {
+        return playerStats.spirituality;
+    }
+
+    public int GetExperience()
+    {
+        return playerStats.experience;
     }
 }
